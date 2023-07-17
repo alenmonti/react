80b2250bbad592ededbc54e9277e7c4ef4cc49ec
alenmonti <90219740+alenmonti@users.noreply.github.com>
2023-07-17 03:34:43 -0300

    usestate slider

--- ecommerce/src/components/Hero.jsx
+++ ecommerce/src/components/Hero.jsx
@@ -9,11 +9,17 @@ const carousel = document.getElementById("carousel");
 
 
 const Hero = () => {
-    const [current, setCurrent] = useState(2);
+    const [current, setCurrent] = useState(0);
+    const next = () => {
+        setCurrent((curr) => (curr === imageList.length - 1 ? 0 : curr + 1))
+    }
+    const prev = () => {
+        setCurrent((curr) => (curr === 0 ? imageList.length - 1 : curr - 1))
+    }
     return (
         <div className="relative w-full">
-            <div className="w-[85%] mx-auto overflow-hidden">
-                <div id="carousel" className="flex flex-nowrap gap-20" style={{transform: `translateX(${current * 1800})px`}}>
+            <div className="w-[85%] mx-auto overflow-hidden relative">
+                <div id="carousel" className="flex flex-nowrap gap-20" style={{transform: `translateX(-${current * 1800})px`}}>
                     {imageList.map((image, index) => (
                         <img src={image} alt={`Hero ${index}`} key={index} className="w-[1600px] mx-auto"/>
                     ))}
